1430bc6b908b2f5de24e663f10e29e30f28ccf7c
sami.allouche <samuel5.allouche@gmail.com>
2023-12-19 18:31:17 +0200

    i added fifo in the program i need to check for the type of the name to decide if to exuxate the fifo out

--- filter.vhd
+++ filter.vhd
@@ -18,11 +18,52 @@ architecture beh of tftp_filte_part is
     signal data_type : Element := (others => (others => '0'));
 	 --signal ver_port_des          : std_logic_vector(7 downto 0);
 	 signal cnt_bus               : integer:= 0;
+	 signal cnt_fifo               : integer:= 0;
+	 signal cnt1_fifo               : integer:= 0;
+
+
 	 --signal bus_temp              : std_logic_vector(7 downto 0);
-	signal process_flag :std_logic;
+	signal flag_finish_pack :std_logic:='0';
 	signal cnt_byte              : integer:= 0;
 	--signal ver_opcode            : std_logic_vector(7 downto 0);
+
+	component fifo_part
+	PORT
+	(
+		clock		: IN STD_LOGIC ;
+		data		: IN STD_LOGIC_VECTOR (7 DOWNTO 0);
+		rdreq		: IN STD_LOGIC ;
+		sclr		: IN STD_LOGIC ;
+		wrreq		: IN STD_LOGIC ;
+		empty		: OUT STD_LOGIC ;
+		full		: OUT STD_LOGIC ;
+		almost_full : OUT STD_LOGIC ;
+		q		: OUT STD_LOGIC_VECTOR (7 DOWNTO 0)
+	);
+end component;
+	signal read_req:std_logic:='0';
+	signal write_req:std_logic:='0';
+	signal fifo_empty:std_logic;
+	signal fifo_full:std_logic;
+	signal d_out:STD_LOGIC_vector(7 downto 0);
+	signal d_in:STD_LOGIC_vector(7 downto 0);
+	signal upload_max:std_logic;
+	signal fifo_reset:std_logic;
+	
+
 begin
+fifo_part_inst : fifo_part PORT MAP (
+		clock	 => clk,
+		data	 => d_in,
+		rdreq	 => read_req,
+		sclr	 => fifo_reset,
+		wrreq	 => write_req,
+		empty	 => fifo_empty,
+		full	 => fifo_full,
+		almost_full => upload_max,
+		q	 => d_out
+	);
+
     process (reset, clk)
         --variable cnt_bus               : integer:= 0;
         --variable cnt_byte              : integer:= 0;
@@ -41,14 +82,11 @@ begin
         if reset = '1' then
             cnt_bus <= 0;
             cnt_byte <= 0;
-			process_flag <='0';
-
+				flag_finish_pack <='0';
             cnt_lenghe_data := 0;
             bus_temp := (others => 'U');
             ver_port_des := (others => '0');
-				--:
             ver_opcode := (others => '0');
-				--:
             length_total := 0;
             length_data := 0;
             cnt_type_data := 0;
@@ -59,12 +97,10 @@ begin
             data_type <= (others => (others => '0'));
         elsif rising_edge(clk) then
 		  if data_in/="UU" then
-		  		process_flag <='0';
             bus_temp(cnt_bus) := data_in(0);
             bus_temp(cnt_bus + 1) := data_in(1);
 					if cnt_bus = 6 then
 						cnt_bus <= 0;
-						--process_flag <='1';
 						bus_test<=bus_temp;
 						if cnt_byte = 46 then
                     ver_port_des := bus_temp;
@@ -89,14 +125,57 @@ begin
                           data_type(cnt_type_data) <= bus_temp;
                            cnt_type_data := cnt_type_data + 1;
 						   end if;
+						elsif(cnt_byte = (48+length_total-5)) and flag_port_cur = '1' and flag_opcode_cur = '1' then
+							flag_finish_pack<='1';
 							end if;
 						  cnt_byte<=cnt_byte+1;
 					else
-						process_flag <='0';
 						cnt_bus <= cnt_bus +	2;
 					end if;
-				data_out <= data_in;
-			end if;	
-		end if;	
+			end if;
+		end if;
     end process;
+	 fifo_process:process(clk,flag_finish_pack,reset)
+	 variable bus_temp_fifo  : std_logic_vector(7 downto 0);
+	 variable flag_restart_fifo:std_logic:='1';
+	 begin
+	 if reset='1' then
+		cnt_fifo <= 0;
+		cnt1_fifo<=0;
+		write_req<='0';
+		fifo_reset <='1';
+		flag_restart_fifo:='1';
+		bus_temp_fifo := (others => 'U');
+	 elsif (rising_edge(clk)) then
+		fifo_reset<='0';
+	   if((flag_finish_pack='0' or upload_max='0') ) then
+		
+			if data_in/="UU" then
+				write_req <= '0';
+            bus_temp_fifo(cnt_fifo) := data_in(0);
+            bus_temp_fifo(cnt_fifo + 1) := data_in(1);
+					if cnt_fifo = 6 then
+					cnt_fifo<=0;
+					write_req <= '1';
+					d_in <=bus_temp_fifo;
+					end if;
+					cnt_fifo <= cnt_fifo +	2;
+			end if;
+		elsif (((flag_finish_pack='1') or (upload_max='1') )and flag_restart_fifo='1')  then 
+			if fifo_empty='1' then
+				fifo_reset<='1';
+				flag_restart_fifo:='0';
+			end if;
+			read_req<='1';
+			data_out(0)<= d_out(cnt1_fifo);
+			data_out(1)<= d_out(cnt1_fifo+1);
+			if cnt1_fifo=6 then
+				cnt1_fifo<=0;
+				read_req<='0';
+			end if;
+			cnt1_fifo<=cnt1_fifo+2;
+			
+		end if;
+		end if;
+	 end process;
 end beh;
