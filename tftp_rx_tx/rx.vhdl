34daf3adbe5f975b1340c8e598cd23cb00b8e85b
sami.allouche <samuel5.allouche@gmail.com>
2024-02-28 19:56:10 +0200

    \

--- tftp_rx_tx/rx.vhdl
+++ tftp_rx_tx/rx.vhdl
@@ -12,75 +12,72 @@ tx_out:out std_logic_vector(1 downto 0));
 end entity;
 
 architecture beh of tftp_rmii_rx is
- 
-signal data_arr:bit;
 signal last_crs:std_logic:='1';
-signal check_mode:integer:=0;
-signal cnt_num_of_zero:integer:=0;
-
+signal cnt1:integer:=0;
+signal ok:integer:=0;
+signal delay_one_clk:integer:=0;
+signal cnt:integer:=0;
+SIGNAL check_mode:integer:=0;
+SIGNAL clear_mode :integer:=0;
  begin
 	 check_valid_data:process(clk,crs)
-	 variable cnt1:integer:=0;
-	variable ok:integer:=0;
-	 	 variable cnt:integer:=0;
-		 
-	 begin
-	 if crs='1' then
-			if rx_d="01" and ok=1 then
-				data_arr<='1';
-				cnt1:=cnt1+1;
-			elsif rx_d="00" then
-				ok:=1;
-			end if;
-		end if;
-
-	if crs='0' and cnt1>8 then
-		check_mode<=1;
-	end if;
-	if rising_edge(clk) then
-		if check_mode=1 then
-			if crs/=last_crs and last_crs='0' then
-			
-	--if (check_mode=1) and (clk='1') then 
-		--if crs='0' then
-			--cnt_num_of_zero<=cnt_num_of_zero+1;
-		--end if;
-		--if crs='1' then
-		--	cnt_num_of_zero<=cnt_num_of_zero-1;
-		--end if;
-		--if crs/=last_crs then
-			--last_crs<=crs;
-		--elsif crs=last_crs and last_crs='0' then
-			data_arr<='0';
-			ok:=0;
-			check_mode<=0;
-			cnt_num_of_zero<=0;
-			cnt:=0;
-			cnt1:=0;
-			end if;
-		end if;
-	end if;
-	if crs='0' and cnt1<2 then
-			data_arr<='0';
-			ok:=0;
-			check_mode<=0;
-			cnt_num_of_zero<=0;
-			cnt:=0;
-			cnt1:=0;
-	end if;
-	end process;
+ 	variable cnt_num_of_zero:integer:=0;
+	variable data_arr:bit:='0';
+	VARIABLE buff_crs:std_logic_vector(1 downto 0):=(others=>'0');
 	
- transmit_data:process(clk)
- begin
-	if data_arr='1' and clk='1' then	 
-			tx_out<=rx_d;
-			tx_ok<='1';
-	end if;
-	if data_arr='0' and clk='1'then	
-			tx_out<="00";
-			tx_ok<='0';	
-	end if;
-end process;
- end beh;
- 
- 
+	variable buff_data:std_logic_vector(1 downto 0):=(others => '0');
+	 begin		
+			 if rising_edge(clk) then
+					if crs='1' then
+						if rx_d="01" and ok=1 then
+						data_arr:='1';
+							delay_one_clk<=1;
+							cnt1<=cnt1+1;
+						elsif rx_d="00" then
+							ok<=1;
+						end if;
+					end if;
+					--------------------------------------------
+					buff_crs:=crs & buff_crs(1);
+					--------------------------------------------
+					if check_mode=1 then	
+						if buff_crs="00" then 
+							data_arr:='0';
+							clear_mode<=1;
+						end if;
+					end if;
+					------------------------------------------
+					if cnt1>8 then
+						if crs='0' then
+							check_mode<=1;
+						end if;
+					end if;
+			-----------------------------------------------
+					if clear_mode=1 then
+						data_arr:='0';
+						ok<=0;
+						check_mode<=0;
+						clear_mode<=0;
+						cnt_num_of_zero:=0;
+						cnt<=0;
+						cnt1<=0;
+					end if;
+			------------------------------------
+				if data_arr='1' then	
+						tx_out<=buff_data;
+						buff_data:=rx_d;
+						--tx_out<=rx_d;
+						tx_ok<='1';
+				end if;
+				if data_arr='0' then
+						tx_out<=buff_data;
+						buff_data:="00";
+						--tx_out<="00";
+						tx_ok<='0';	
+				end if;	
+		-------------------------RISING_EDGE
+			end if;
+	end process;
+end beh;
+		 
+		 
