dd33f1539f08c09c8ded6c7031c1edd7c6cbe826
sami.allouche <samuel5.allouche@gmail.com>
2024-04-29 14:27:59 +0300

    ds

--- software/filter.c
+++ software/filter.c
@@ -9,8 +9,24 @@
 #define HW_REGS_BASE 0xFF200000
 #define HW_REGS_SPAN 0x00200000
 
+
+void reverseString(char *inputString) {
+    int length = strlen(inputString);  // Find the length of the string
+    int i, j = length - 1;
+    char temp;
+
+    // Swap characters starting from the ends moving towards the center
+    for (i = 0; i < length / 2; i++) {
+        temp = inputString[i];
+        inputString[i] = inputString[j];
+        inputString[j] = temp;
+        j--;
+    }
+}
+
 int main() {
     int fd;
+    int cnt=0;
     void *virtual_base;
     volatile uint32_t *filter_addr;
     char file_type[5] = {0};  
@@ -31,12 +47,18 @@ int main() {
         close(fd);
         return -1;
     }
-
+    printf("Welcome to the tftp filter- a game changer in the network security");
     while (1) {
-        printf("Enter command (add, delete, reset, exit): ");
+        printf("What action would you like to do in the filter: (add+, delete-, reset X, exit ->, show » ) ");
         scanf("%s", command);
 
         if (strcmp(command, "exit") == 0) {
+             for (int i = 0; i < 4; i++) {
+                current_types[i] = 0;
+                filter_addr = (volatile uint32_t *)(virtual_base + i * 4);
+                *filter_addr = 0;
+            }
+            printf("You are exiting the system \n");
             break;
         }
 
@@ -51,13 +73,15 @@ int main() {
         else if (strcmp(command, "add") == 0) {
             printf("Enter a file type to filter: ");
             scanf("%4s", file_type);
+            reverseString(file_type);
             int added = 0;
             for (int i = 0; i < 4; i++) {
                 if (current_types[i] == 0) {
                     current_types[i] = *(uint32_t *)file_type;
                     filter_addr = (volatile uint32_t *)(virtual_base +  i * 4);
                     *filter_addr = current_types[i];
-                    printf("File type '%s' added to the filter.\n", file_type);
+                    reverseString(file_type);
+                    printf("File type '%s' added to the filter.\n",file_type);
                     added = 1;
                     break;
                 }
@@ -66,23 +90,41 @@ int main() {
                 printf("Error: No available. Delete an existing type first. we can filter up to 4 types!\n");
             }
         } else if (strcmp(command, "delete") == 0) {
-            printf("Enter a file type to delete from the filter: ");
+            printf("Enter a file type to delete from the filter: \n ");
             scanf("%4s", file_type);
+            reverseString(file_type);
             int found = 0;
             for (int i = 0; i < 4; i++) {
                 if (current_types[i] == *(uint32_t *)file_type) {
                     current_types[i] = 0;
                     filter_addr = (volatile uint32_t *)(virtual_base +  i * 4);
                     *filter_addr = 0;
-                    printf("File type '%s' deleted.\n", file_type);
+                    reverseString(file_type);
+                    printf("File type '%s' deleted.\n",file_type);
                     found = 1;
                     break;
                 }
             }
             if (!found) {
-                printf("File type '%s' not found.\n", file_type);
+                printf("File type '%s' not found.\n",file_type);
             }
-        } else {
+        }
+           else if (strcmp(command, "show") == 0) {
+            printf("All the type of data that you are already filtering: \n");
+            for (int i = 0; i < 4; i++) {
+                if (current_types[i] != 0) {
+                    printf("file type: '%s'\n",file_type);
+                  }
+                else{
+                cnt++;
+                if (cnt==4) {
+                    printf("You are currently not filtering anything!! \n");
+                    cnt=0;
+                }
+                }
+            } 
+        }
+         else {
             printf("Invalid command.\n");
         }
     }
--- tftp_rx_tx/top_2way.vhdl
+++ tftp_rx_tx/top_2way.vhdl
@@ -1,9 +1,8 @@
 library ieee;
 use ieee.std_logic_1164.all;
-
 entity way2_rmii is
     port (
-		led_test :out std_logic;
+		led_test : out std_logic;
         clk      : in  std_logic;
         clk_out  : out std_logic;
         rx_d_0   : in  std_logic_vector(1 downto 0);
@@ -16,10 +15,10 @@ entity way2_rmii is
         crs_1    : in  std_logic;
         tx_en_1  : out std_logic;
         tx_d_1   : out std_logic_vector(1 downto 0);
-		  reset :in std_logic;
+		reset    : in std_logic;
 		  --------------------------------------------------
-		  		memory_mem_a1                           : out   std_logic_vector(14 downto 0);                    -- mem_a
-            memory_mem_ba1                           : out   std_logic_vector(2 downto 0);                     -- mem_ba
+		  	memory_mem_a1                           : out   std_logic_vector(14 downto 0);                    -- mem_a
+            memory_mem_ba1                          : out   std_logic_vector(2 downto 0);                     -- mem_ba
             memory_mem_ck1                          : out   std_logic;                                        -- mem_ck
             memory_mem_ck_n1                        : out   std_logic;                                        -- mem_ck_n
             memory_mem_cke1                         : out   std_logic;                                        -- mem_cke
@@ -33,9 +32,7 @@ entity way2_rmii is
             memory_mem_dqs_n1                       : inout std_logic_vector(3 downto 0)  := (others => 'X'); -- mem_dqs_n
             memory_mem_odt1                         : out   std_logic;                                        -- mem_odt
             memory_mem_dm1                          : out   std_logic_vector(3 downto 0);                     
-            memory_oct_rzqin1                       : in    std_logic                     := 'X'
-		  ---------------------------------------------------
-    );
+            memory_oct_rzqin1                       : in    std_logic                     := 'X');
 end entity way2_rmii;
 
 architecture beh1 of way2_rmii is
@@ -72,28 +69,28 @@ architecture beh1 of way2_rmii is
 
     component top_rmii is 
         port (
-		  led_test :out std_logic;
-            clk   : in  std_logic;
-            rx_d  : in  std_logic_vector(1 downto 0);
-            rx_er : in  std_logic;
-            crs   : in  std_logic;
-            tx_en_f : out std_logic;
-            tx_d_f  : out std_logic_vector(1 downto 0);
-				 data_in0 :in std_logic_vector(31 downto 0):=(others=>'0');
-				 data_in1 : in std_logic_vector(31 downto 0):=(others=>'0');
-				 data_in2 : in std_logic_vector(31 downto 0):=(others=>'0');
-				 data_in3 : in std_logic_vector(31 downto 0):=(others=>'0'));
+		       led_test  :out std_logic;
+             clk       : in  std_logic;
+             rx_d      : in  std_logic_vector(1 downto 0);
+             rx_er     : in  std_logic;
+             crs       : in  std_logic;
+             tx_en_f   : out std_logic;
+             tx_d_f    : out std_logic_vector(1 downto 0);
+				 data_in0  :in std_logic_vector(31 downto 0):=(others=>'0');
+				 data_in1  : in std_logic_vector(31 downto 0):=(others=>'0');
+				 data_in2  : in std_logic_vector(31 downto 0):=(others=>'0');
+				 data_in3  : in std_logic_vector(31 downto 0):=(others=>'0'));
     end component;
 			signal	 data_0 :std_logic_vector(31 downto 0):=(others=>'0');
 			signal	 data_1 :std_logic_vector(31 downto 0):=(others=>'0');
 			signal	 data_2 :std_logic_vector(31 downto 0):=(others=>'0');
 			signal	 data_3 :std_logic_vector(31 downto 0):=(others=>'0');
 begin
-    --------------------------------
+    ----------------------------------------
     clk_out <= clk;
-    -------------------------------------
+    ----------------------------------------
 	 
-	 ----------------------------------------------------
+	 --------------------------------------------------
 		U0: soc_system PORT MAP (
 		clk_clk => clk,
 		custom_filter_0_filter_type_new_signal =>data_0,
@@ -101,7 +98,7 @@ begin
 		custom_filter_0_filter_type_new_sig=>data_2,
 		custom_filter_0_filter_type_new_si =>data_3,
 		reset_reset_n => reset, 
-	----------------------------------------------------
+	---------------------------------------------------
 				memory_mem_a                           => memory_mem_a1,                                     
             memory_mem_ba                          => memory_mem_ba1,                          
             memory_mem_ck                          => memory_mem_ck1,                          
@@ -123,32 +120,30 @@ begin
 	----------------------------------------------------
 
     zero2one : top_rmii port map (
-	 led_test=>led_output,
-			data_in0 =>data_0,
-		data_in1 =>data_1,
-		data_in2 =>data_2,
-		data_in3 =>data_3,
-        clk   => clk,
-        rx_d  => rx_d_0,
-        rx_er => rx_er_0,
-        crs   => crs_0,
-        tx_en_f => tx_en_1,
-        tx_d_f  => tx_d_1
-    );
+	   led_test      =>led_output,
+		data_in0      =>data_0,
+		data_in1      =>data_1,
+		data_in2      =>data_2,
+		data_in3      =>data_3,
+      clk           => clk,
+      rx_d          => rx_d_0,
+      rx_er         => rx_er_0,
+      crs           => crs_0,
+      tx_en_f       => tx_en_1,
+      tx_d_f        => tx_d_1);
 
     one2zero : top_rmii port map (
-	 	 led_test=>led_output1,
-		data_in0 =>data_0,
-		data_in1 =>data_1,
-		data_in2 =>data_2,
-		data_in3 =>data_3,
-        clk   => clk,
-        rx_d  => rx_d_1,
-        rx_er => rx_er_1,
-        crs   => crs_1,
-        tx_en_f => tx_en_0,
-        tx_d_f  => tx_d_0 
-    );
+	 	 led_test => led_output1,
+		 data_in0 => data_0,
+		 data_in1 => data_1,
+		 data_in2 => data_2,
+		 data_in3 => data_3,
+       clk      => clk,
+       rx_d     => rx_d_1,
+       rx_er    => rx_er_1,
+       crs      => crs_1,
+       tx_en_f  => tx_en_0,
+       tx_d_f   => tx_d_0 );
 	 
 	 led_test<= led_output1 or led_output;
 end beh1;
